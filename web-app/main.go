9fee6063a602349b2e60ed41b1f24ff9e040de13
Víctor Peinado <vitojph@gmail.com>
2021-08-24 18:52:25 +0200

    Render pages with basic templates

--- web-app/main.go
+++ web-app/main.go
@@ -1,42 +1,57 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+var homeTemplate *template.Template
+var contactTemplate *template.Template
+var notFoundTemplate *template.Template
+
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>¡Bienvenido a mi web!</h1>")
-	fmt.Fprint(w, "<p>Es mía, sí.</p>")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Contacto</h1>")
-	fmt.Fprint(w, "<p>Escríbeme un email a <a href=\"mailto:contact@mail.com\">esta dirección</a>.</p>")
-}
-
-func faq(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Preguntas Frecuentes</h1>")
-	fmt.Fprint(w, "<p>Esta es mi lista de preguntas frecuentes.</p>")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func notFound404(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>404 Personalizada</h1>")
-	fmt.Fprint(w, "<p>Lo siento, página no encontrada.</p>")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	if err := notFoundTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
+	var err error
+	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	notFoundTemplate, err = template.ParseFiles("views/404.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
 	var handler404 http.Handler = http.HandlerFunc(notFound404)
+
 	r := mux.NewRouter()
 	r.NotFoundHandler = handler404
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":8080", r)
 }
