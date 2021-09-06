f0ac7a031af813fee13cfc8d196669dc72fd1ddb
Víctor Peinado <vitojph@gmail.com>
2021-09-06 23:43:17 +0200

    Parse a form

--- controllers/users.go
+++ controllers/users.go
@@ -6,14 +6,19 @@ import (
 	"web-dev-go/views"
 )
 
+type Users struct {
+	NewView *views.View
+}
+
+type SignupForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
 func NewUsers() *Users {
 	return &Users{
 		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
 	}
-}
-
-type Users struct {
-	NewView *views.View
 }
 
 // New is used to render the form where a user can create a new user account
@@ -27,5 +32,10 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 // Create is used to process the signup for when a uses tries to create a new user account
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "This is a temporary response")
+	var form SignupForm
+	if err := parseForm(r, &form); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, "Email is", form.Email)
+	fmt.Fprintln(w, "Pass is", form.Password)
 }
